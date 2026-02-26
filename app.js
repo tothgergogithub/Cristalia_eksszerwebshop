584bee9ee83ae890c11af8786abdedf69d438fd9
urbandavi <urbandavidpeter@turr.hu>
2026-02-26 12:27:12 +0100

    SCRUM-29 app.listen fix

--- app.js
+++ app.js
@@ -40,6 +40,11 @@ app.get('/nyaklancok.html', (req, res) => {
 app.get('/rolunk.html', (req, res) => {
     res.sendFile(path.join(__dirname, "html", 'rolunk.html'));
 });
-app.listen(app.listen(3000, () => {
+
+
+
+
+
+app.listen(3000, () => {
     console.log('http://localhost:3000');
-}))
+})
